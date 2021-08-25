51c8f5d9c4048e50cd6e9bb2a26adf52efee1542
Rafael Cunha <rnascunha@gmail.com>
2021-08-25 14:49:57 -0300

    Added "A stanley tale" site

---
+++ a-stanley-tale/graph.js
@@ -0,0 +1,175 @@
+
+let main = document.querySelector("main");
+
+function getContentWidth (element)
+{
+  let styles = getComputedStyle(element)
+
+  return element.clientWidth
+    - parseFloat(styles.paddingLeft)
+    - parseFloat(styles.paddingRight)
+}
+
+let margin = {top: 20, right: 15, bottom: 30, left: 30},
+  width = getContentWidth(main) - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+let parseDate = d3.timeParse("%H:%M:%S");
+
+let x = d3.scaleTime().range([0, width]);
+let y = d3.scaleLinear().range([height, 0]);
+
+let valueStanley = d3.line()
+                .x(function(d){ return x(d.time); })
+                .y(function(d){ return y(d.stanley); }),
+    valueEnv = d3.line()
+                .x(function(d){ return x(d.time); })
+                .y(function(d){ return y(d.env); }),
+    valueControl = d3.line()
+                .x(function(d){ return x(d.time); })
+                .y(function(d){ return y(d.control); });
+
+let svg = d3.select("#chart").append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+              .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+d3.select("#chart").append("figcaption").text("Temperature comparsion");
+
+let focus = svg.append("g")
+    .style("display", "none");
+let bisectDate = d3.bisector(function(d) { return d.time; }).left;
+
+d3.tsv('data.tsv').then((data) => {
+  data.forEach(d => {
+      d.time = parseDate(d.time);
+      d.stanley = +d.stanley.replace(',','.');
+      d.env = +d.env.replace(',','.');
+      d.control = +d.control.replace(',','.');
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.time; }));
+  y.domain([0, d3.max(data, function(d) { return Math.max(d.stanley, d.env, d.control); })]);
+
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line stanley")
+      .attr("d", valueStanley);
+
+  svg.append("path")
+      .data([data])
+      .attr("class", "line env")
+      .attr("d", valueEnv);
+
+  svg.append("path")
+      .data([data])
+      .attr("class", "line control")
+      .attr("d", valueControl);
+
+  // Add the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%H:%M")));
+
+  // Add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  /**
+   * Legend
+   */
+  var legend = svg.selectAll('g')
+      .data(['stanley', 'env', 'control'], d => d)
+      .enter()
+      .append('g')
+      .attr('class', 'legend');
+
+  legend.append('rect')
+    .attr('x', width - 75)
+    .attr('y', function(d, i) {
+      return height - ((3 - i) * 20) - 12;
+    })
+    .attr('width', 10)
+    .attr('height', 10)
+    .attr('class', function(d){ return d; });
+
+  legend.append('text')
+    .attr('x', width - 55)
+    .attr('y', function(d, i) {
+      return height - ((3 - i) * 20);
+    })
+    .text(function(d) {
+      return d;
+    });
+
+    /**
+     *
+     */
+     focus.append("path") // this is the black vertical line to follow mouse
+      .attr("class", "mouse-line")
+      .style("opacity", "0");
+
+    /*
+     * Pointer
+     */
+    // append the circle at the intersection
+    focus.selectAll('g')
+        .data(['stanley', 'env', 'control'], d => d)
+        .enter()
+        .append("circle")
+        .attr("class", (d) => `y ${d}`)
+        .style("fill", "none")
+        .style("stroke-width", "2px")
+        .attr("r", 4);
+
+    // append the rectangle to capture mouse
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .on("mouseover", function() {
+          svg.select(".mouse-line")
+            .style("opacity", "1");
+          focus.style("display", null);
+        })
+        .on("mouseout", function() {
+          focus.style("display", "none");
+          svg.select(".mouse-line")
+            .style("opacity", "0");
+        })
+        .on("mousemove", mousemove);
+
+    function mousemove()
+    {
+  	  let x0 = x.invert(d3.pointer(event, this)[0]),
+  		  i = bisectDate(data, x0, 1),
+  		  d0 = data[i - 1],
+  		  d1 = data[i],
+  		  d = x0 - d0.time > d1.time - x0 ? d1 : d0;
+
+  	     focus.select("circle.y.stanley")
+  	      .attr("transform",
+  	         "translate(" + x(d.time) + "," +
+                            y(0.88 * d.stanley) + ")");
+          focus.select("circle.y.env")
+           .attr("transform",
+              "translate(" + x(d.time) + "," +
+                             y(0.88 * d.env) + ")");
+         focus.select("circle.y.control")
+  	      .attr("transform",
+  	         "translate(" + x(d.time) + "," +
+	                        y(0.88 * d.control) + ")");
+
+          svg.select(".mouse-line")
+            .attr("d", function() {
+              var dm = "M" + x(d.time) + "," + height;
+              dm += " " + x(d.time) + "," + 0;
+              return dm;
+            });
+  	}
+});
---
+++ a-stanley-tale/graph_diff.js
@@ -0,0 +1,162 @@
+
+// let main = document.querySelector("main");
+
+// function getContentWidth (element)
+// {
+//   let styles = getComputedStyle(element)
+//
+//   return element.clientWidth
+//     - parseFloat(styles.paddingLeft)
+//     - parseFloat(styles.paddingRight)
+// }
+//
+// let margin = {top: 20, right: 15, bottom: 30, left: 30},
+//   width = getContentWidth(main) - margin.left - margin.right,
+//   height = 400 - margin.top - margin.bottom;
+
+// let parseDate = d3.timeParse("%H:%M:%S");
+
+// let x = d3.scaleTime().range([0, width]);
+// let y = d3.scaleLinear().range([height, 0]);
+
+let valueStanleyDiff = d3.line()
+                .x(function(d){ return x(d.time); })
+                .y(function(d){ return y(d.diff_stanley); }),
+    valueControlDiff = d3.line()
+                .x(function(d){ return x(d.time); })
+                .y(function(d){ return y(d.diff_control); });
+
+let svg_diff = d3.select("#chart_diff").append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+              .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+d3.select("#chart_diff").append("figcaption").text("Temperature difference to envirioment");
+
+let focus_diff = svg_diff.append("g")
+        .style("display", "none");
+
+d3.tsv('data.tsv').then((data) => {
+  data.forEach(d => {
+      d.time = parseDate(d.time);
+      d.diff_stanley = +d.diff_stanley.replace(',','.');
+      d.diff_control = +d.diff_control.replace(',','.');
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.time; }));
+  y.domain([0, d3.max(data, function(d) { return Math.max(d.diff_stanley, d.diff_control); })]);
+
+  // Add the valueline path.
+  svg_diff.append("path")
+      .data([data])
+      .attr("class", "line stanley")
+      .attr("d", valueStanleyDiff);
+
+  svg_diff.append("path")
+      .data([data])
+      .attr("class", "line control")
+      .attr("d", valueControlDiff);
+
+  // let axisFormat =
+  // Add the x Axis
+  svg_diff.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%H:%M")));
+
+  // Add the y Axis
+  svg_diff.append("g")
+      .call(d3.axisLeft(y));
+
+  /**
+   * Legend
+   */
+  var legend = svg_diff.selectAll('g')
+      .data(['stanley', 'control'], d => d)
+      .enter()
+      .append('g')
+      .attr('class', 'legend');
+
+  legend.append('rect')
+    .attr('x', width - 75)
+    .attr('y', function(d, i) {
+      return (i * 20) - 12;
+    })
+    .attr('width', 10)
+    .attr('height', 10)
+    .attr('class', function(d){ return d; });
+
+  legend.append('text')
+    .attr('x', width - 55)
+    .attr('y', function(d, i) {
+      return (i * 20);
+    })
+    .text(function(d) {
+      return d;
+    });
+
+    /**
+     *
+     */
+     focus_diff.append("path") // this is the black vertical line to follow mouse
+      .attr("class", "mouse-line")
+      .style("opacity", "0");
+
+    /*
+     * Pointer
+     */
+    // append the circle at the intersection
+    focus_diff.selectAll('g')
+        .data(['stanley', 'control'], d => d)
+        .enter()
+        .append("circle")
+        .attr("class", (d) => `y ${d}`)
+        .style("fill", "none")
+        .style("stroke-width", "2px")
+        .attr("r", 4);
+
+    // append the rectangle to capture mouse
+    svg_diff.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .on("mouseover", function() {
+          svg_diff.select(".mouse-line")
+            .style("opacity", "1");
+          focus_diff.style("display", null);
+        })
+        .on("mouseout", function() {
+          focus_diff.style("display", "none");
+          svg_diff.select(".mouse-line")
+            .style("opacity", "0");
+        })
+        .on("mousemove", mousemove);
+
+    function mousemove()
+    {
+  	  let x0 = x.invert(d3.pointer(event, this)[0]),
+  		  i = bisectDate(data, x0, 1),
+  		  d0 = data[i - 1],
+  		  d1 = data[i],
+  		  d = x0 - d0.time > d1.time - x0 ? d1 : d0;
+
+  	     focus_diff.select("circle.y.stanley")
+  	      .attr("transform",
+  	         "translate(" + x(d.time) + "," +
+                            y(d.diff_stanley) + ")");
+         focus_diff.select("circle.y.control")
+  	      .attr("transform",
+  	         "translate(" + x(d.time) + "," +
+	                        y(d.diff_control) + ")");
+
+          svg_diff.select(".mouse-line")
+            .attr("d", function() {
+              var dm = "M" + x(d.time) + "," + height;
+              dm += " " + x(d.time) + "," + 0;
+              return dm;
+            });
+  	}
+});
